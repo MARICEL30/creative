c0df41da5ffeeef4f6b7ea00bdf5b7462c6d5b6a
Maricel Dragan <42100507+MARICEL30@users.noreply.github.com>
2020-08-17 11:41:24 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+   function openNav() {
+        
+        document.getElementById("mySidebar").style.width = "400px";
+        document.getElementById("main").style.marginLeft = "400px";
+        document.getElementById("headline").style.color = "white";
+        
+        }
+        
+        function closeNav() {
+            
+            document.getElementById("mySidebar").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+             document.getElementById("headline").style.color = "black";
+             
+        }
